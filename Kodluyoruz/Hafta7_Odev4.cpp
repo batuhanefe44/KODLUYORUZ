701dc4ba187f290795354c7fed016f14a82f895e
Batuhan Efe Kuşçu <91881938+batuhanefe44@users.noreply.github.com>
2023-07-09 00:45:19 +0300

    Hafta7_Odev4

---
+++ Kodluyoruz/Hafta7_Odev4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int decimalNumber;
+
+    printf("Bir ondalik sayi girin: ");
+    scanf("%d", &decimalNumber);
+
+    printf("Onaltilik biçimde: %x\n", decimalNumber);
+
+    return 0;
+}
